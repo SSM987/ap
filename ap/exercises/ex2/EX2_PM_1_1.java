88b0c80c3e1b2a656f41a0e3542605aa6b332a0b
SSM987 <sphrmsavi385@gmail.com>
2025-03-24 16:37:37 +0330

    EX2_PM_1_1

---
+++ ap/exercises/ex2/EX2_PM_1_1.java
@@ -0,0 +1,25 @@
+package ap.exercises.ex2;
+
+import java.util.Scanner;
+
+public class EX2_PM_1_1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter a number:");
+        int k = s.nextInt();
+        for(int i = 0; i<k+2; i++)
+            System.out.print('*');
+        System.out.println();
+        for(int i=0;i<k;i++) {
+            for (int j = 0; j < k + 3; j++) {
+                if (j == 0 || j == k + 2)
+                    System.out.print('*');
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i<k+2; i++)
+            System.out.print('*');
+    }
+}
