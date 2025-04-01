8702ac5c83bcb0a2448865400ae2f8497e118397
SSM987 <sphrmsavi385@gmail.com>
2025-04-01 17:19:09 +0330

    EX3_LM_1_1

---
+++ ap/exercises/ex3/EX3_LM_1_1.java
@@ -0,0 +1,59 @@
+package ap.exercises.ex3;
+class Book {
+    String bookName;
+    String author;
+    int numberOfPages;
+    int year;
+
+    public Book(String bookName, String author, int numberOFPages, int year) {
+        this.bookName = bookName;
+        this.author = author;
+        this.numberOfPages = numberOFPages;
+        this.year = year;
+    }
+
+    public void DisplayBookInfo() {
+        System.out.println("bookname: " + bookName+"\t\t"+"author: " + author+"\t\t"+"number of pages: " + numberOfPages+"\t\t"+"year: " + year);
+    }
+    public void ChangeBookInfo(String bookName, String author, int numberOFPages, int year){
+        this.bookName = bookName;
+        this.author = author;
+        this.numberOfPages = numberOFPages;
+        this.year = year;
+    }
+}
+class Student{
+    String firstName;
+    String lastname;
+    int studentNumber;
+    String fieldOfStudy;
+
+    public Student(String firstName,String lastname,int studentNumber,String fieldOfStudy){
+        this.fieldOfStudy=fieldOfStudy;
+        this.firstName=firstName;
+        this.lastname=lastname;
+        this.studentNumber=studentNumber;
+    }
+    public void DisplayStudentInfo(){
+        System.out.println("firstName: "+firstName+"\t\t"+"lastname: "+lastname+"\t\t"+"studentNumber: "+studentNumber+"\t\t"+"fieldOfStudy: "+fieldOfStudy);
+    }
+    public void ChangeStudentInfo(String firstName,String lastname,int studentNumber,String fieldOfStudy){
+        this.fieldOfStudy=fieldOfStudy;
+        this.firstName=firstName;
+        this.lastname=lastname;
+        this.studentNumber=studentNumber;
+    }
+}
+public class EX3_LM_1_1 {
+    public static void main(String[] args) {
+        Book book1= new Book("The selfish GENE","Richard dockins",253,1967);
+        Book book2 = new Book("The big project","Steven Havking", 358,2010);
+        Student student1= new Student("Sepehr","Afshar",21552568 ,"computer engineering");
+        Student student2= new Student("Ali","Molaeei",54156256 ,"mechanic engineering");
+        book1.DisplayBookInfo();
+        book2.DisplayBookInfo();
+        student1.DisplayStudentInfo();
+        student2.DisplayStudentInfo();
+    }
+    }
+
