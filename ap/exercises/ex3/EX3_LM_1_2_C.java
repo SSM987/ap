566821dff63183ee03fdc1b177a48a693b255f71
SSM987 <sphrmsavi385@gmail.com>
2025-04-07 16:58:31 +0330

    EX3_LM_1_2_C

---
+++ ap/exercises/ex3/EX3_LM_1_2_C.java
@@ -0,0 +1,23 @@
+package ap.exercises.ex3;
+
+public class EX3_LM_1_2_C {
+    public static void main(String[] args) {
+        Student[] students = new Student[3];
+        students[0] = new Student("Sepehr", "Afshar", 21552568, "computer engineering");
+        students[1] = new Student("Ali", "Molaeei", 54156256, "mechanic engineering");
+        students[2] = new Student("Mohammad", "Hasani", 12345678, "Mathematics");
+        Book[] books = new Book[4];
+        books[0] = new Book("The selfish GENE", "Richard dockins", 253, 1967);
+        books[1] = new Book("The big project", "Steven Havking", 358, 2010);
+        books[2] = new Book("Effective Java", "Joshua Bloch", 416, 2018);
+        books[3] = new Book("Clean code", "Robert C.Martin", 464, 2008);
+        EX3_LM_1_2_A.SaveStudentsToFile(students);
+        EX3_LM_1_2_A.SaveBooksToFile(books);
+        Student[] showStudent = EX3_LM_1_2_B.readStudentsFromFile();
+        Book[] showBooks = EX3_LM_1_2_B.readBookFromFile();
+        for (Student student:showStudent)
+            student.DisplayStudentInfo();
+        for (Book book:showBooks)
+            book.DisplayBookInfo();
+    }
+}
