2cce4e66e2f536d4f90e7f844fc9c22f56b840d3
SSM987 <sphrmsavi385@gmail.com>
2025-04-07 15:11:14 +0330

    EX3_LM_1_2_A

--- ap/exercises/ex3/EX3_LM_1_2_A.java
+++ ap/exercises/ex3/EX3_LM_1_2_A.java
@@ -4,21 +4,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class EX3_LM_1_2_A {
-    public static void main(String[] args) {
-        Book[] books={
-                new Book("The selfish GENE","Richard dockins",253,1967),
-                new Book("The big project","Steven Havking", 358,2010),
-                new Book("Effective Java","Joshua Bloch",416,2018),
-                new Book("Clean code","Robert C.Martin",464,2008),
-        };
-        Student[] students={
-                new Student("Sepehr","Afshar",21552568 ,"computer engineering"),
-                new Student("Ali","Molaeei",54156256 ,"mechanic engineering"),
-                new Student("Mohammad","Hasani",12345678,"Mathematics")
-        };
-        SaveBooksToFile(books);
-        SaveStudentsToFile(students);
-    }
     public static void SaveBooksToFile(Book[]books){
         try(FileWriter fileWriter=new FileWriter("Books.txt")) {
             for (Book book : books)
